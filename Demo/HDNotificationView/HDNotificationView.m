0027940ac057322a685b2b9f022b5b831004bc3d
Chanchal Raj <rajchanchalkhatri@gmail.com>
2016-07-01 13:56:45 +0500

    Changed Time of notification to 7 seconds

--- Demo/HDNotificationView/HDNotificationView.m
+++ Demo/HDNotificationView/HDNotificationView.m
@@ -22,7 +22,7 @@
 #define LABEL_MESSAGE_FRAME                     CGRectMake(45.0f, 25.0f, [[UIScreen mainScreen] bounds].size.width - 45.0f, LABEL_MESSAGE_FRAME_HEIGHT)
 #define LABEL_MESSAGE_FRAME_WITHOUT_IMAGE       CGRectMake(5.0f, 25.0f, [[UIScreen mainScreen] bounds].size.width - 5.0f, LABEL_MESSAGE_FRAME_HEIGHT)
 
-#define NOTIFICATION_VIEW_SHOWING_DURATION                  1200.0f    /// second(s)
+#define NOTIFICATION_VIEW_SHOWING_DURATION                  7.0f    /// second(s)
 #define NOTIFICATION_VIEW_SHOWING_ANIMATION_TIME            0.5f    /// second(s)
 
 @implementation HDNotificationView
