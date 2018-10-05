14580b025220f2f28a703c673a1fd0bb24d61340
Dang Nguyen <>
2018-10-05 09:06:53 +0700

    [INIT] New source version 2.0.0 in Swift

---
+++ HDNotificationView/HDNotificationView.h
@@ -0,0 +1,19 @@
+//
+//  HDNotificationView.h
+//  HDNotificationView
+//
+//  Created by VN_LW70130-M on 10/5/18.
+//  Copyright © 2018 AnG Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDNotificationView.
+FOUNDATION_EXPORT double HDNotificationViewVersionNumber;
+
+//! Project version string for HDNotificationView.
+FOUNDATION_EXPORT const unsigned char HDNotificationViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDNotificationView/PublicHeader.h>
+
+
